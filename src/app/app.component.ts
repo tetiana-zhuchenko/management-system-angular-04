7c23878ff1fc8538209fa99a8990b776dd1c473c
Tania Zhuchenko <taniyhamail@gmail.com>
2024-08-13 10:48:08 +0300

    feat: refactored dashboard items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashbord/server-status/server-status.component';
 import { TicketsComponent } from './dashbord/tickets/tickets.component';
 import { TrafficComponent } from './dashbord/traffic/traffic.component';
+import { DashboardItemComponent } from './dashbord/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { TrafficComponent } from './dashbord/traffic/traffic.component';
     ServerStatusComponent,
     TicketsComponent,
     TrafficComponent,
+    DashboardItemComponent,
   ],
 })
 export class AppComponent {}
