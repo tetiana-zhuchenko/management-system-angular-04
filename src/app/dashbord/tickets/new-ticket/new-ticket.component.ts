8b3708893fa997fcde7b963684bf705509afc94f
Tania Zhuchenko <taniyhamail@gmail.com>
2024-08-15 14:56:24 +0300

    feat: added "viewChild" and "viewChildren"

--- src/app/dashbord/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashbord/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  viewChild,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,8 +17,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // @ViewChildren(ButtonComponent) buttons?: [] //<-- uses for select multiple elements
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
   onSubmit(title: string, text: string) {
     console.log(title);
     console.log(text);
+    this.form().nativeElement.reset();
   }
 }
--- src/app/dashbord/tickets/tickets.component.ts
+++ src/app/dashbord/tickets/tickets.component.ts
@@ -4,7 +4,7 @@ import { NewTicketComponent } from './new-ticket/new-ticket.component';
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent, NewTicketComponent],
+  imports: [NewTicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
