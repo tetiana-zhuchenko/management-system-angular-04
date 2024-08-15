805f4180da21f1b67352d54e48d91a8ae00ab46e
Tania Zhuchenko <taniyhamail@gmail.com>
2024-08-15 14:15:35 +0300

    feat: added onDestroy and OnInit methods

--- src/app/dashbord/server-status/server-status.component.ts
+++ src/app/dashbord/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  // private interval?: NodeJS.Timeout
+  private interval?: ReturnType<typeof setInterval>;
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
 }
