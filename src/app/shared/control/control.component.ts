91a2390fbbdb718548fad8d8a4946dc99ea3a058
Tania Zhuchenko <taniyhamail@gmail.com>
2024-08-15 13:02:24 +0300

    feat: added "host" practice

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,7 +14,22 @@ import { Component, input } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    // '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'; // do the same as "host: {class: 'control' }" in @Component decorator
+  @HostListener('click') onClick() {
+    console.log('clicked');
+    console.log(this.el);
+  }
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  // onClick() {
+  //   console.log('Clicked!');
+  // }
 }
